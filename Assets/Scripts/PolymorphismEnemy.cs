9e59c1a88da24905d42eb77b219f2a6d158149a8
syaringame <tamifool.game@gmail.com>
2026-02-08 17:21:00 +0900

    polymophism-scene
    - add Enemy
    - add PolimophismEnemy

---
+++ Assets/Scripts/PolymorphismEnemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PolymorphismEnemy : Enemy
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    public override void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0f, v) * 2;
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+
+    public override void Jump()
+    {
+        if (Rb != null)
+        {
+            Rb.AddForce(Vector3.up * jumpForce * 2, ForceMode.Impulse);
+        }
+    }
+}
